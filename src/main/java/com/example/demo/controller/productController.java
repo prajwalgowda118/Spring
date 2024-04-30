3edd399af4223115dab3769f429f10ebb82b2053
prajwalgowda118 <prajwal7683@gmail.comgit config --global user.name prajwalgowda118git config --global user.email prajwal7683@gmail.com>
2024-04-30 14:16:01 +0100

    this is my new commit

---
+++ src/main/java/com/example/demo/controller/productController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class productController {
+
+        @GetMapping("/hello")
+        public String hello(){
+
+            return "Hello World";
+        }
+}
